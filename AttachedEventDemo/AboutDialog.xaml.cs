e2e49b2db86d515a7b258911b5cef7616ef60318
Shingi Mutandwa <shingi.mutandwa@gmail.com>
2009-06-11 20:37:53 +1200

    initial commit

---
+++ AttachedEventDemo/AboutDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttachedEventDemo {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (e.AddedItems.Count > 0)
+                MessageBox.Show("You just selected " + e.AddedItems[0]);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("You just clicked " + e.Source);
+        }
+
+        private void GenericHandler(object sender, RoutedEventArgs e) {
+            if (e.RoutedEvent == Button.ClickEvent)
+                MessageBox.Show("You just clicked " + e.Source);
+            else if (e.RoutedEvent == ListBox.SelectionChangedEvent) {
+                SelectionChangedEventArgs scvg = (SelectionChangedEventArgs)e;
+                if (scvg.AddedItems.Count > 0)
+                    MessageBox.Show("You just selected " + scvg.AddedItems[0]);
+            }
+            else
+                MessageBox.Show("Unknown event.");
+        }
+    }
+}
---
+++ RoutedEventDemo/AboutDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEventDemo {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class AboutDialog : Window {
+        public AboutDialog() {
+            InitializeComponent();
+            //AddHandler(Window.MouseRightButtonDownEvent, new MouseButtonEventHandler(Window_MouseRightButtonDown), true);
+        }
+
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
+            this.Title = "Source = " + e.Source.GetType().Name + ", OriginalSource = " + e.OriginalSource.GetType().Name + " @ " + e.Timestamp;
+            Control source = e.Source as Control;
+            if (source.BorderThickness != new Thickness(5)) {
+                source.BorderThickness = new Thickness(5);
+                source.BorderBrush = Brushes.Black;
+            }
+            else
+                source.BorderThickness = new Thickness(0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("button clicked");
+        }
+    }
+}
